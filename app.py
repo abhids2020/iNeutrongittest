74140546ae02cf4799ff869925711ac85e05b3a5
abhishek gaurav <gabhi.net@gmail.com>
2022-10-18 19:30:29 +0530

    added docker file

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print('hello')
+
+import numpy as np
+import pandas as pd
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def home():
+    return "Hello world"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
